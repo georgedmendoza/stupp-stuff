096f6ae8309788573bcbb7ed00396a60d4fe1a95
george mendoza <george.mendoza@hrblock.com>
2022-03-28 19:34:58 -0500

    image path

--- src/app/services/carousel.service.ts
+++ src/app/services/carousel.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Carousel} from '../models/carousel/carousel'
 // import '../../assets/carousel.json';
+// ./carousle
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class CarouselService {
 
   constructor(private http: HttpClient) { }
   getCarousel() {
-    return this.http.get<any>('../../assets/carousel.json')
+    return this.http.get<any>('./carousel.json')
       .toPromise()
       .then(res => <Carousel[]>res.data)
       .then(data => { return data; });
